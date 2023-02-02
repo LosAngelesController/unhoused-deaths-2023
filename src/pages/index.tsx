17638646dd80d686dd53a83f53cf13bc6e6107dc
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-02-02 13:21:32 -0800

    add datadog sample

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,6 +27,9 @@ const councildistricts = require("./CouncilDistricts.json");
 const citybounds = require("./citybounds.json");
 // @ts-ignore: Unreachable code error
 import * as turf from "@turf/turf";
+import { datadogRum } from '@datadog/browser-rum';
+
+
 
 // added the following 6 lines.
 import mapboxgl from "mapbox-gl";
@@ -180,6 +183,27 @@ const Home: NextPage = () => {
   const [isLoggedIn, setisLoggedIn] = useState(false);
 
   const [user, loading, error] = useAuthState(auth);
+
+    const datadogconfig:any = {
+      applicationId: '54ed9846-68b0-4811-a47a-7330cf1828a0',
+      clientToken: 'pub428d48e3143310cf6a9dd00003773f12',
+      site: 'datadoghq.com',
+      service:'311homeless',
+      env:'prod',
+      // Specify a version number to identify the deployed version of your application in Datadog 
+      // version: '1.0.0',
+      
+      sessionSampleRate: 100,
+      sessionReplaySampleRate: 100,
+      trackUserInteractions: true,
+      trackResources: true,
+      trackLongTasks: true,
+      defaultPrivacyLevel:'allow'
+  }
+
+  datadogRum.init(datadogconfig);
+    
+datadogRum.startSessionReplayRecording();
 
   useEffect(() => {
     if (loading) {
@@ -1023,39 +1047,37 @@ const Home: NextPage = () => {
 
             <div className="absolute mt-[7.9em] md:mt-[5.8em] ml-2 md:ml-3 top-0 z-5 flex flex-row gap-x-2">
               {isLoggedIn === true && (
-              <>
-                <button
-                  onClick={() => {
-                    setfilterpanelopened(!filterpanelopened);
-                  }}
-                  className="mt-2 rounded-full px-3 pb-1.5 pt-0.5 text-sm bold md:text-base bg-gray-800 bg-opacity-80 text-white border-white border-2"
-                >
-                  <svg
-                    style={{
-                      width: "20px",
-                      height: "20px",
+                <>
+                  <button
+                    onClick={() => {
+                      setfilterpanelopened(!filterpanelopened);
                     }}
-                    viewBox="0 0 24 24"
-                    className="inline align-middle mt-0.5"
+                    className="mt-2 rounded-full px-3 pb-1.5 pt-0.5 text-sm bold md:text-base bg-gray-800 bg-opacity-80 text-white border-white border-2"
                   >
-                    <path
-                      fill="currentColor"
-                      d="M14,12V19.88C14.04,20.18 13.94,20.5 13.71,20.71C13.32,21.1 12.69,21.1 12.3,20.71L10.29,18.7C10.06,18.47 9.96,18.16 10,17.87V12H9.97L4.21,4.62C3.87,4.19 3.95,3.56 4.38,3.22C4.57,3.08 4.78,3 5,3V3H19V3C19.22,3 19.43,3.08 19.62,3.22C20.05,3.56 20.13,4.19 19.79,4.62L14.03,12H14Z"
-                    />
-                  </svg>
-                  <span>Filter</span>
-                </button>
-                <button
-                onClick={() => {
-                  signOutOfApp();
-                }}
-                className="mt-2 rounded-full px-3 pb-1.5 pt-0.5 text-sm bold md:text-base bg-gray-800 bg-opacity-80 text-white border-white border-2"
-              >
-                
-                <span>Logout</span>
-              </button>
-              
-              </>
+                    <svg
+                      style={{
+                        width: "20px",
+                        height: "20px",
+                      }}
+                      viewBox="0 0 24 24"
+                      className="inline align-middle mt-0.5"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M14,12V19.88C14.04,20.18 13.94,20.5 13.71,20.71C13.32,21.1 12.69,21.1 12.3,20.71L10.29,18.7C10.06,18.47 9.96,18.16 10,17.87V12H9.97L4.21,4.62C3.87,4.19 3.95,3.56 4.38,3.22C4.57,3.08 4.78,3 5,3V3H19V3C19.22,3 19.43,3.08 19.62,3.22C20.05,3.56 20.13,4.19 19.79,4.62L14.03,12H14Z"
+                      />
+                    </svg>
+                    <span>Filter</span>
+                  </button>
+                  <button
+                    onClick={() => {
+                      signOutOfApp();
+                    }}
+                    className="mt-2 rounded-full px-3 pb-1.5 pt-0.5 text-sm bold md:text-base bg-gray-800 bg-opacity-80 text-white border-white border-2"
+                  >
+                    <span>Logout</span>
+                  </button>
+                </>
               )}
             </div>
 
