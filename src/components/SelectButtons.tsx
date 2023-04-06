cbde9125c376cd6d5ab67f1f131d977ef269fc0a
na0nie <naonchun@gmail.com>
2023-04-06 14:33:59 -0700

    select buttons

--- src/components/SelectButtons.tsx
+++ src/components/SelectButtons.tsx
@@ -9,25 +9,25 @@ interface onClickReqs {
 
 export function SelectButtons(props: onClickReqs) {
   return (
-      <div className="flex flex-row gap-x-1">
-        <button
-          className="align-middle bg-gray-800 rounded-lg px-1  border border-gray-400 text-sm md:text-base"
-          onClick={props.onSelect}
-        >
-          Select All
-        </button>
-        <button
-          className="align-middle bg-gray-800 rounded-lg px-1 text-sm md:text-base border border-gray-400"
-          onClick={props.onUnselect}
-        >
-          Unselect All
-        </button>
-        <button
-          onClick={props.onInvert}
-          className="align-middle bg-gray-800 rounded-lg px-1 text-sm md:text-base  border border-gray-400"
-        >
-          Invert
-        </button>
-      </div>
+    <div className="flex flex-row gap-x-1">
+      <button
+        className="align-middle bg-gray-800 rounded-lg px-1  border border-gray-400 text-sm md:text-base"
+        onClick={props.onSelect}
+      >
+        Select All
+      </button>
+      <button
+        className="align-middle bg-gray-800 rounded-lg px-1 text-sm md:text-base border border-gray-400"
+        onClick={props.onUnselect}
+      >
+        Unselect All
+      </button>
+      <button
+        onClick={props.onInvert}
+        className="align-middle bg-gray-800 rounded-lg px-1 text-sm md:text-base  border border-gray-400"
+      >
+        Invert
+      </button>
+    </div>
   );
 }
