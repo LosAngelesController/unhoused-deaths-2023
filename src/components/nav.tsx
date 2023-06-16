45ffb9492505971a98233d6745730cc60799e288
na0nie <naonchun@gmail.com>
2023-06-16 10:23:31 -0700

    map guide message box

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,4 +1,5 @@
 import ActiveLink from "./ActiveLink";
+
 const navigationPayroll = [
   {
     name: "41.18",
@@ -13,12 +14,18 @@ const navigationPayroll = [
 ];
 
 function Nav() {
+  const messageBox = () => {
+    alert(
+      "-Click & Drag to explore locations on the map, or enter a location in the Search bar.\n\n-Click on a map point to view arrest details by location.\n\n-Click Filter to view by Race, Year, Council District, Arrest Type, and Time."
+    );
+  };
+
   return (
     <div className="z-50 bg-[#1a1a1a] flex flex-col">
       <nav className="z-50 flex flex-row  h-content">
         {navigationPayroll.map((item: any, itemIdx: any) => (
           <ActiveLink
-            activeClassName="text-white  py-2  md:py-3 px-6 block hover:text-green-300 focus:outline-none text-green-300 border-b-2 font-medium border-green-300"
+            activeClassName="text-white py-2 md:py-3 px-6 block hover:text-green-300 focus:outline-none text-green-300 border-b-2 font-medium border-green-300"
             href={item.url}
             key={itemIdx}
             target={`${item.newtab === true ? "_blank" : ""}`}
@@ -28,6 +35,12 @@ function Nav() {
             </p>
           </ActiveLink>
         ))}
+        <p
+          className="text-white py-2 text-sm md:text-base   md:py-3 px-3 block hover:text-green-300 focus:outline-none underline"
+          onClick={messageBox}
+        >
+          Map Guide
+        </p>
       </nav>
     </div>
   );
