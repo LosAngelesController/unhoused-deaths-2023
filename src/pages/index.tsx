1bad0df9963b7e7612d78022687b5f51ccc40122
na0nie <naonchun@gmail.com>
2023-08-14 12:08:37 -0700

    minor css change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1539,8 +1539,8 @@ const Home: NextPage = () => {
                           <strong>Evictions by Top 20 Zip Codes</strong>
                         </p>
                         <p className="text-[#41ffca] text-xs mt-1">
-                          <strong className="text-red-600">RESET</strong> map to view Category, Notice, and
-                          CD filters
+                          <strong className="text-red-600">RESET</strong> map to
+                          view Category, Notice, and CD filters
                         </p>
                       </div>
                     </div>
