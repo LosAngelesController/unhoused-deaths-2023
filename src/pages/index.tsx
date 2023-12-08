2f005ee25da9c56f13d7090f7b2f5c8adefdda09
na0nie <naonchun@gmail.com>
2023-12-08 14:21:59 -0800

    minor edit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -156,11 +156,7 @@ const Home: NextPage = () => {
       mapref.current.getSource("linestring-hover")
     );
 
-    mapref.current.setLayoutProperty(
-      "line-selected",
-      "visibility",
-      "none"
-    );
+    mapref.current.setLayoutProperty("line-selected", "visibility", "none");
 
     setEvictionInfoOpen(false);
     if (mapref) {
@@ -552,11 +548,7 @@ const Home: NextPage = () => {
         var evictionPoint: any = map.getSource("linestring-hover");
         evictionPoint.setData(e.features[0].geometry);
 
-        map.setLayoutProperty(
-          "line-selected",
-          "visibility",
-          "visible"
-        );
+        map.setLayoutProperty("line-selected", "visibility", "visible");
 
         setEvictionData(filteredData);
       });
@@ -899,7 +891,6 @@ const Home: NextPage = () => {
                   <button
                     onClick={() => {
                       setselectedfilteropened("district");
-                      setFilteredZipCodesPre([]);
                     }}
                     className={`px-2 border-b-2  py-1  font-semibold ${
                       selectedfilteropened === "district"
